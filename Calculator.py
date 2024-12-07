5d8a2341e1cd892739861a4da41b7a1cfe2960cc
TheophaneChrist <theophane7@outlook.de>
2024-12-07 15:31:08 +0100

    Calculator_Script_Based

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,7 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton , QVBoxLayout, QHBoxLayout, QGridLayout, QLineEdit
+from PyQt5.QtCore import QCoreApplication, QEvent
+
 
 import random as rdn
 from nltk.corpus import words
@@ -35,10 +37,49 @@ main_Layout_1.addWidget(Calc_text,0,0)
 # create all App Objects
 
 Buttons = [ "AC" , "+/-" , "%" , "/", "7", "8", "9", "*", "4" , "5", "6" , 
-           "-", "1", "2", "3", "+" , "0", "="]
+           "-", "1", "2", "3", "+" , "0","<", "="]
 
-#Buttons = [ "7" , "8" , "9" , "/", "4", "5", "6", "*", "1" , "2", "3" , 
-#           "+", "1", "2", "3", "+" , "0", "="]
+
+
+def button_click():
+    button = main_window.sender()
+    text   = button.text()
+
+    if text == "=":
+        symbol = Calc_text.text()
+        try:
+            res = eval(symbol)
+            Calc_text.setText(str(res))
+        except Exception as e:
+            print("Error:", e)
+
+    elif text == "AC":                      # Clear button
+        Calc_text.clear()
+    
+    elif text == "%" :
+        current_value = Calc_text.text()
+        current_value = eval(current_value)
+        Calc_text.setText(str(current_value/100))
+
+    elif text == "+/-"  :
+        current_value = Calc_text.text()
+        current_value = eval(current_value)
+        Calc_text.setText(str(-1*current_value))
+
+    elif text == "<" :
+        current_value = Calc_text.text()
+        Calc_text.setText(current_value[:-1])                  # Delete button
+
+    else:
+        current_value = Calc_text.text()
+        Calc_text.setText( current_value + text)
+
+
+
+
+
+
+
 
 def is_number(value):
     try:
@@ -56,16 +97,18 @@ but_num = 0 ;
 Button_calc =[]
 for button_calc in Buttons:
     Button_calc.append( QPushButton(button_calc) ) 
+    Button_calc[but_num].clicked.connect(button_click)
     main_Layout.addWidget(Button_calc[but_num], rows,col)  # zeile row, column col
-    col                 = col     + 1 
+    col      += 1
     if col > 3:
         col = 0 
         rows  = rows+1
 
-    if is_number(button_calc):
-        Button_calc[but_num].clicked.connect(lambda: Calc_text.setPlaceholderText(Button_calc[but_num].text()))
-
-    but_num              = but_num  + 1
+#    if is_number(button_calc):
+        #Button_calc[but_num].clicked.connect(lambda: Calc_text.setPlaceholderText(Button_calc[but_num].text()))
+        #Button_calc[but_num].clicked.connect(lambda but_num=but_num: Calc_text.setPlaceholderText(Button_calc[but_num].text()))
+#        Button_calc[but_num].clicked.connect(lambda _, but_num=but_num: Calc_text.setPlaceholderText(Button_calc[but_num].text()))
+    but_num  += 1 
 
 
 
@@ -73,26 +116,9 @@ but_num = 0 ;
 
 master_Layout.addLayout(main_Layout_1) 
 master_Layout.addLayout(main_Layout) 
-
 main_window.setLayout(master_Layout)
 
 
-
-
-#for pos_numb in np.arange(0,len(Buttons),1):
- #   if is_number(Button_calc[pos_numb].text()):
- #       Button_calc[pos_numb].clicked.connect(lambda: Calc_text.setPlaceholderText(str(Button_calc[pos_numb].text())))
-
-#Button_calc[4].clicked.connect(lambda: Calc_text.setPlaceholderText("7"))
-#Button_calc[5].clicked.connect(lambda: Calc_text.setPlaceholderText("8"))
-#Button_calc[6].clicked.connect(lambda: Calc_text.setPlaceholderText("9"))
-#Button_calc[8].clicked.connect(lambda: Calc_text.setPlaceholderText("4"))
-#Button_calc[9].clicked.connect(lambda: Calc_text.setPlaceholderText("5"))
-#Button_calc[10].clicked.connect(lambda: Calc_text.setPlaceholderText("6"))
-#Button_calc[12].clicked.connect(lambda: Calc_text.setPlaceholderText("1"))
-#Button_calc[13].clicked.connect(lambda: Calc_text.setPlaceholderText("2"))
-#Button_calc[14].clicked.connect(lambda: Calc_text.setPlaceholderText("3"))
-#Button_calc[16].clicked.connect(lambda: Calc_text.setPlaceholderText("0"))
 
 
 # Show/ Run our App
